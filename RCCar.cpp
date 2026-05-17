3c2e9c2993036c74ad5af11b10b1754dc73b7fe1
hc_iterator <13482012512@163.com>
2026-05-17 18:12:07 +0800

    给拓展课老师展示实力的代码完成 	modified:   RCCar.cpp

--- RCCar.cpp
+++ RCCar.cpp
@@ -1,8 +1,8 @@
 #include <pico/stdlib.h>
 #include "TM1637.h"
 
-#define TM1637_CLK 2
-#define TM1637_DIO 3
+#define TM1637_CLK 4
+#define TM1637_DIO 5
 
 int main() {
     TM1637 tm(TM1637_CLK, TM1637_DIO);
@@ -10,7 +10,6 @@ int main() {
 
     int minute = 0;
     int second = 0;
-    bool point_on = true;
 
     while (true) {
         int8_t data[4];
@@ -18,14 +17,20 @@ int main() {
         data[1] = minute % 10;
         data[2] = second / 10;
         data[3] = second % 10;
-
-        // 小数点反相：每半秒切换，亮灭交替
-        tm.point(point_on);
+        
         tm.display(data);
-        point_on = !point_on;
 
-        // 首次显示后等半秒再更新数字
+        sleep_ms(250);
+
+        tm.point(true);
+        tm.display(data);
+
         sleep_ms(500);
+
+        tm.point(false);
+        tm.display(data);
+
+        sleep_ms(249);
 
         // 关闭小数点
         tm.point(false);
@@ -41,7 +46,6 @@ int main() {
             }
         }
 
-        sleep_ms(500);  // 半秒后进入下一轮
     }
 
     return 0;
